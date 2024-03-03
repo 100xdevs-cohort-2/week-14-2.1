d56f164c52dbdc7d845256af3ed19dd8fae90e12
harkirat singh <harkiratsingh@harkirats-MacBook-Pro-2.local>
2024-03-03 13:38:48 +0530

    week-14-2.1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { UserCard } from './components/UserCard'
+
+function App() {
+
+  return (
+    <>
+      <UserCard />
+    </>
+  )
+}
+
+export default App
